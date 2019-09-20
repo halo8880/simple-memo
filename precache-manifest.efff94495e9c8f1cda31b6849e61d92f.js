1667724e44734d61ce47b6cf94b8e82de1d9bc88
vvhoang <vvhoang@mgmgerrit.mgm-tp.com>
2019-09-20 12:57:01 +0700

    Updates

---
+++ precache-manifest.efff94495e9c8f1cda31b6849e61d92f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e307d75bd787e3c749bfe1bda1dc203",
+    "url": "/halo8880/simple-memo/index.html"
+  },
+  {
+    "revision": "2e4dc86f3acf30fb10e0",
+    "url": "/halo8880/simple-memo/static/css/main.9bad68fe.chunk.css"
+  },
+  {
+    "revision": "c2c609d5401dd2c0fb51",
+    "url": "/halo8880/simple-memo/static/js/2.b075873a.chunk.js"
+  },
+  {
+    "revision": "2e4dc86f3acf30fb10e0",
+    "url": "/halo8880/simple-memo/static/js/main.9a6e8298.chunk.js"
+  },
+  {
+    "revision": "1d18122376dfe3952537",
+    "url": "/halo8880/simple-memo/static/js/runtime~main.bdd018cf.js"
+  }
+]);
